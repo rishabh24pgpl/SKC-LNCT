90afba267f5c7a781e0a6adfb478d10e713efe5b
sumitPGPL <sumitpgpl@gmail.com>
2024-04-13 17:01:25 +0530

    nav bar

---
+++ app/components/RelatedLink/RelatedLink.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function RelatedLink() {
+  return (
+    <>
+      <div className="border-2 w-[400px] p-1 text-black ">
+       <div className=" shadow-[rgba(100,_0,_0,_0.24)_0px_3px_8px] px-4 p-2 text-xl  font-semibold " > Related-Link</div>
+      
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+       <h1 className="text-black p-2 border-b-2 w-96 mx-auto  hover:text-blue-800 "> Academic Reguation</h1>
+      
+
+       
+
+      
+
+       <h1></h1>
+      </div>
+    </>
+  );
+}
