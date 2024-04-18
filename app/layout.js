d1d732abfd4e31a52e31d6e3317464025c16e7f0
Harshal22pgpl <harshalpgpl@gmail.com>
2024-04-18 15:24:00 +0530

    navbar mdifies

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
