2eda5a22d896018d2ebf7c6afb6960394225b3cc
sumitPGPL <sumitpgpl@gmail.com>
2024-05-06 13:31:44 +0530

    ui updateee

--- app/components/AbouttheInstitute/AbouttheInstitute.jsx
+++ app/components/AbouttheInstitute/AbouttheInstitute.jsx
@@ -4,37 +4,37 @@ import React from "react";
 export default function AbouttheInstitute() {
   return (
     <>
-      <div className=" flex justify-around bg-blue-700">
-        <div className="   w-full h-full">
+      <div className="  w-11/12 mx-auto mt-10 justify-around bg-blue-700">
+        <div className="   ">
           <h1 className="text-3xl pt-10 pb-5 px-10 ">About the Institute</h1>
-          <p className=" text-justify px-10 pb-4">
+          <p className="  px-10 pb-4">
             NIT Rourkela is one of the premier national level institutions for
             technical education in the country and is funded by the Government
             of India. The Government of India has elevated the Regional
             Engineering College, Rourkela to a university under the name of
             National Institute of Technology, Rourkela.
           </p>
-          <p className=" text-justify px-10 pb-5 border-b  ">
+          <p className="  px-10 pb-5 border-b  ">
             The main objective of the Institute is to produce quality Engineers
             and Scientists in Graduate and Post-Graduate levels in various
             branches of Engineering and Science. The Institute is managed by
             the.
           </p>
-          <div className="flex">
-            <div className=" p-5 flex items-start">
-              <Image
+          <div className="flex ">
+            <div className=" p-10  flex items-start">
+              {/* <Image
                 src="/mission.png"
                 alt="mission img"
                 width={80}
                 height={80}
-              />
+              /> */}
 
               <div>
 
               <h1 className="text-2xl font-semibold text-center mb-2 ">
-                mission
+                MISSION
               </h1>
-              <p className=" p-2 text-justify">
+              <p className=" p-2 ">
               To become an internationally acclaimed institution of higher
                 learning that will serve as a source of knowledge and expertise
                 for the society and be a preferred destination for undergraduate
@@ -42,19 +42,19 @@ export default function AbouttheInstitute() {
               </p>
               </div>
             </div>
-            <div className=" p-5 flex items-start">
-              <Image
+            <div className=" p-10 flex items-start">
+              {/* <Image
                 src="/vision.png"
                 alt="vision img"
                 width={80}
                 height={80}
-              />
+              /> */}
 
              <div>
               <h1 className="text-2xl  font-semibold text-center mb-2">
-                Vision
+                VISION
               </h1>
-              <p className=" p-2  text-justify mb-4">
+              <p className=" p-2   mb-4">
                 To become an internationally acclaimed institution of higher
                 learning that will serve as a source of knowledge and expertise
                 for the society and be a preferred destination for undergraduate
@@ -65,11 +65,11 @@ export default function AbouttheInstitute() {
           </div>
         </div>
 
-        <div className="  justify-end items-center w-full">
-          <h1 className="text-3xl pt-10 px-10  ">Message from Director</h1>
-          <div className=" flex justify-around pt-10 p-8">
+          <h1 className="text-4xl  border-t p-4 px-10  ">Message from Director</h1>
+        <div className=" flex justify-between items-center w-full">
+          <div className=" flex justify-between pt-1 px-10">
             <div className="w-full">
-            <p className=" p-2  text-pretty  ">
+            <p className=" p-2   text-pretty  ">
               To become an internationally acclaimed institution of higher
               learning that will serve as a source of knowledge and expertise
               for the society and be a preferred destination for undergraduate
@@ -79,9 +79,15 @@ export default function AbouttheInstitute() {
               and graduate studiesTo become an internationally acclaimed
               institution of higher learning that will serve as a source of
               knowledge and expertise for the society and be .
+              and graduate studiesTo become an internationally acclaimed
+              institution of higher learning that will serve as a source of
+              knowledge and expertise for the society and be .
+              and graduate studiesTo become an internationally acclaimed
+              institution of higher learning that will serve as a source of
+              knowledge and expertise for the society and be .
             </p>
             </div>
-            <div className="w-full  ">
+            <div className="w-full  flex justify-around items-end ">
             <Image src="/founder.png" alt="" height={230} width={400} />
             </div>
           </div>
