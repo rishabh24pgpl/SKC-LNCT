45b2fbf867beed02344c4d01e87e50be84e9c194
Harshal22pgpl <harshalpgpl@gmail.com>
2024-05-16 16:58:00 +0530

    bhasadsolve

---
+++ app/new/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TextRevealCard } from '../components/TextRevealCard/TextRevealCard'
+
+export default function page() {
+  return (
+    <div>
+      <TextRevealCard text="My Name is Harshal" revealText="I am Harshal Khairnar"/>
+    </div>
+  )
+}
