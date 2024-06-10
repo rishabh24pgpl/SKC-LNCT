c23164567d80cc80bfdf3a5709721201bb26afdf
unknown <rishabh.pgpl@gmail.com>
2024-06-10 11:26:57 +0530

    navbar fixes

--- app/components/Button/Button.jsx
+++ app/components/Button/Button.jsx
@@ -1,41 +1,41 @@
-import React, { forwardRef } from "react";
-import clsx from "clsx";
+// import React, { forwardRef } from "react";
+// import clsx from "clsx";
 
-const Types = {
-  primary: "btn-primary",
-  secondary: "btn-secondary",
-  tertiary: "btn-tertiary",
-  delete: "btn-delete",
-};
+// const Types = {
+//   primary: "btn-primary",
+//   secondary: "btn-secondary",
+//   tertiary: "btn-tertiary",
+//   delete: "btn-delete",
+// };
 
-const Sizes = {
-  large: "text-button",
-  small: "text-button-sm",
-};
+// const Sizes = {
+//   large: "text-button",
+//   small: "text-button-sm",
+// };
 
-const Button = forwardRef((props, ref) => {
-  const {
-    className,
-    btnType = "primary",
-    size = "large",
-    onClick,
-    children,
-    disabled,
-    backgroundColor,
-    ...otherProps
-  } = props;
+// const Button = forwardRef((props, ref) => {
+//   const {
+//     className,
+//     btnType = "primary",
+//     size = "large",
+//     onClick,
+//     children,
+//     disabled,
+//     backgroundColor,
+//     ...otherProps
+//   } = props;
 
-  return (
-    <button
-      ref={ref}
-      onClick={onClick}
-      className={clsx("btn", Types[btnType], Sizes[size], className)}
-      disabled={disabled}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
-});
+//   return (
+//     <button
+//       ref={ref}
+//       onClick={onClick}
+//       className={clsx("btn", Types[btnType], Sizes[size], className)}
+//       disabled={disabled}
+//       {...otherProps}
+//     >
+//       {children}
+//     </button>
+//   );
+// });
 
-export default Button;
+// export default Button;
